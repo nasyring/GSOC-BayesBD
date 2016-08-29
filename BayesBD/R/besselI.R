5e33c34bd0712e485b247ff22161ac5097b0301d
nasyring <nasyrin@gmail.com>
2016-08-29 09:22:51 -0400

    Add files via upload

---
+++ BayesBD/R/besselI.R
@@ -0,0 +1,5 @@
+besselI <-
+function (x, nu, expon.scaled = FALSE) 
+{
+    .Internal(besselI(x, nu, 1 + as.logical(expon.scaled)))
+}
---
+++ BayesBD/R/plotBD.R
@@ -0,0 +1,23 @@
+plotBD = function(obs, samples){
+		par(mfrow = c(1, 2))
+		rotate <- function(x) t(apply(x, 2, rev))  # rotate closewise by 90 degrees
+		image(rotate(obs$intensity), axes = FALSE, asp = 1)
+		x = samples$estimate*cos(samples$theta)+obs$center[1]
+		y = samples$estimate*sin(samples$theta)+obs$center[2]
+		theta.plot = samples$theta
+		plot(x,y, lty = 2, lwd = 3,type="n", axes=FALSE, frame.plot=FALSE, xlab = "", ylab = "")
+		x = samples$upper*cos(samples$theta)+obs$center[1]
+		y = samples$upper*sin(samples$theta)+obs$center[2]
+		polygon(x, y,fillOddEven = TRUE, col = 'gray', border = NA)
+		x = samples$lower*cos(samples$theta)+obs$center[1]
+		y = samples$lower*sin(samples$theta) +obs$center[2]
+		polygon(x, y,fillOddEven = TRUE, col = 'white', border = NA)
+		x = samples$estimate*cos(samples$theta)+obs$center[1]
+		y = samples$estimate*sin(samples$theta)+obs$center[2]
+		lines(x,y, lty = 2, lwd = 3)
+		if(!is.null(obs$gamma.fun)){
+			x = obs$gamma.fun(theta.plot)*cos(theta.plot)+obs$center[1]
+			y = obs$gamma.fun(theta.plot)*sin(theta.plot)+obs$center[2]
+		}
+		lines(x,y, lty = 1, lwd = 1)
+}
