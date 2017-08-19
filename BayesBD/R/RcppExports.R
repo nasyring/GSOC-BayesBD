903e728f6863d6a743196e7f3020130acb635452
nasyring <nasyrin@gmail.com>
2017-08-18 20:17:59 -0500

    Update RcppExports.R

--- BayesBD/R/RcppExports.R
+++ BayesBD/R/RcppExports.R
@@ -5,22 +5,22 @@ BayesBDbinary <- function(obs, inimean, nrun, nburn, J, ordering, mask = rep(1,l
 	obs$r.obs = obs$r.obs[mask==1]
 	obs$theta.obs = obs$theta.obs[mask==1]	    
 	obs$intensity = obs$intensity[mask==1]
-	.Call('BayesBD_BayesBDbinary', PACKAGE = 'BayesBD', obs, inimean, nrun, nburn, J, ordering, slice, outputAll)
+	.Call(BayesBD_BayesBDbinary, obs, inimean, nrun, nburn, J, ordering, slice, outputAll)
 }
 
 BayesBDnormal <- function(obs, inimean, nrun, nburn, J, ordering_mu, ordering_sigma, mask = rep(1,length(obs$intensity)), slice, outputAll) {
     	obs$r.obs = obs$r.obs[mask==1]
 	obs$theta.obs = obs$theta.obs[mask==1]	    
 	obs$intensity = obs$intensity[mask==1]
-	.Call('BayesBD_BayesBDnormal', PACKAGE = 'BayesBD', obs, inimean, nrun, nburn, J, ordering_mu, ordering_sigma, slice, outputAll)
+	.Call(BayesBD_BayesBDnormal, obs, inimean, nrun, nburn, J, ordering_mu, ordering_sigma, slice, outputAll)
 }
 
 eigenfun <- function(n,x) {
-    .Call('BayesBD_eigenfun', PACKAGE = 'BayesBD', n,x)
+    .Call(BayesBD_eigenfun, n,x)
 }
 
 unisliceL <- function(x0, gx0, i_J, tauini, anini, alphalambda, betalambda, lambdaini) {
-    .Call('BayesBD_unisliceL', PACKAGE = 'BayesBD', x0, gx0, i_J, tauini, anini, alphalambda, betalambda, lambdaini)
+    .Call(BayesBD_unisliceL, x0, gx0, i_J, tauini, anini, alphalambda, betalambda, lambdaini)
 }
 
 
